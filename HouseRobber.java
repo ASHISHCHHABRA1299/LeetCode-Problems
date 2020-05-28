113edf748683dccef5f897d53ebe423bb0f47f58
hp <chhabraashish123@gmail.com>
2020-05-28 19:37:47 +0530

    HouseRobberUpdated

--- HouseRobber.java
+++ HouseRobber.java
@@ -1,28 +1,29 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class HouseRobber {
 
 	public static void main(String[] args) {
 
-		int[] nums = { 2, 7, 9, 3, 1 };
-//		int[] nums = { 0 };
-		int i = 0;
-		int maxsum = Integer.MIN_VALUE;
-		while (i < nums.length / 2) {
-			int j = i + 2;
-
-			int sum = nums[i];
-			while (j < nums.length) {
-				sum += nums[j];
-				j = j + 2;
+		int[] nums = { 1, 2, 3, 1 };
+		int[] output = new int[nums.length];
+		Arrays.fill(output, -1);
+		int max = nums[0];
+		output[0] = nums[0];
+		for (int i = 1; i < output.length; i++) {
+			int pos = i - 2;
+			if (pos >= 0) {
+				int max1 = 0;
+				for (int j = pos; j >= 0; j--)
+					max1 = Math.max(max1, output[j]);
+				output[i] = nums[i] + max1;
+			} else {
+				output[i] = nums[i];
 			}
-
-			if (sum > maxsum)
-				maxsum = sum;
-			i = i + 1;
+			max = Math.max(output[i], max);
 		}
-		System.out.println(maxsum);
-
+		System.out.println(max);
 	}
 
 }
