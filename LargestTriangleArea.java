ffb1fb8a0187bc29cbe26c088d8d85cd695c4d6d
hp <chhabraashish123@gmail.com>
2020-06-03 23:53:15 +0530

    LargestTriangleArea

---
+++ LargestTriangleArea.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class LargestTriangleArea {
+
+	public static void main(String[] args) {
+		int[][] points = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 0, 2 }, { 2, 0 } };
+		int N = points.length;
+		double ans = 0;
+		for (int i = 0; i < N; ++i) {
+			for (int j = i + 1; j < N; ++j) {
+				for (int k = j + 1; k < N; ++k) {
+					ans = Math.max(ans, area(points[i], points[j], points[k]));
+				}
+			}
+		}
+		System.out.println(ans);
+
+	}
+
+	public static double area(int[] P, int[] Q, int[] R) {
+		return 0.5 * Math.abs(P[0] * Q[1] + Q[0] * R[1] + R[0] * P[1] - P[1] * Q[0] - Q[1] * R[0] - R[1] * P[0]);
+	}
+
+}
